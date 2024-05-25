917a19392e10ddf0a36acfd47f37944eb05a6017
nishenaryan14 <aryannishen27@gmail.com>
2024-05-26 01:58:25 +0530

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,19 +1,24 @@
-// Loader.js
 import React, { useContext } from "react";
 import "./Loader.css";
 import { AuthContext } from "../context/AuthContext";
 
-const Loader = ({ active, percentage, handleClick }) => {
+const Loader = ({ active, percentage }) => {
   const { currentUser } = useContext(AuthContext);
+
   return (
     <div className={`loader ${active ? "active" : ""}`}>
       <div className="loader-content">
-        <img className="loaderProfile" src={currentUser.photoURL} alt="" />
-
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/chatapp-6b154.appspot.com/o/chat-icon-removebg-preview.png?alt=media&token=d2784dca-65b2-4141-bcb0-8dbdabc157fc"
+          alt=""
+          className="chatLogo"
+        />
+        <p className="loaderLogoText">GoChat</p>
+        <img className="loaderProfile" src={currentUser?.photoURL} alt="" />
         <div className="loader-text">
-          {active ? `Loading ${percentage}%` : "Loading Complete"}
+          {/* {active ? `Loading ${percentage}%` : "Loading Complete"} */}
+          Welcome {currentUser?.displayName}!
         </div>
-        {/* <button onClick={handleClick}>Toggle Loader</button> */}
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,39 @@
 import Sidebar from "../components/Sidebar";
 import Chat from "../components/Chat";
+import Loader from "../components/Loader"; // Import the Loader component
 import { useEffect, useState } from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Home = () => {
   const isSmallScreen = useMediaQuery("(max-width:776px)");
   const [showChat, setShowChat] = useState(false);
+  const [loading, setLoading] = useState(true); // State to manage loader visibility
+  const [percentage, setPercentage] = useState(0); // State to manage loading percentage
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (currentUser) {
+      // Simulate loading process for demo purposes
+      const interval = setInterval(() => {
+        setPercentage((prev) => {
+          if (prev >= 100) {
+            clearInterval(interval);
+            setLoading(false);
+            return 100;
+          }
+          return prev + 1; // Adjust increment value and speed as needed
+        });
+      }, 20); // Adjust the interval duration as needed
+
+      return () => clearInterval(interval);
+    }
+  }, [currentUser]);
+
+  if (loading) {
+    return <Loader active={loading} percentage={percentage} />; // Pass the percentage dynamically
+  }
 
   return (
     <div className="home">
